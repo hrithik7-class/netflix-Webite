f1bb74a39a635f1be7b7491b6be832acf275a72e
hrithik7-class <kesarwaniraj11@gmail.com>
2025-05-24 16:01:26 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 			<div className='flex items-center gap-10 z-50'>
 				<Link to='/'>
 					<img src='/netflix-logo.png' alt='Netflix Logo' className='w-32 sm:w-40' />
-				</Link>
+				</Link> 
 
 				{/* desktop navbar items */}
 				<div className='hidden sm:flex gap-2 items-center'>
